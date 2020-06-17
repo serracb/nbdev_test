ace2d5f81e6888a89ab0aaad7d5a835588f0bad5
serra-tas <jose.camacho-bosca@thalesaleniaspace.com>
2020-06-17 13:53:30 +0200

    images added

--- nbdev_test/_nbdev.py
+++ nbdev_test/_nbdev.py
@@ -2,7 +2,9 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"say_hello": "00_core.ipynb"}
+index = {"say_hello": "00_core.ipynb",
+         "multiply": "00_core.ipynb",
+         "read_image": "00_core.ipynb"}
 
 modules = ["core.py"]
 
