52424b1c5a38985914aaeb0bb22d6b6155805bcc
serra-tas <jose.camacho-bosca@thalesaleniaspace.com>
2020-06-17 14:36:59 +0200

    export_test addition

--- nbdev_test/_nbdev.py
+++ nbdev_test/_nbdev.py
@@ -5,9 +5,11 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 index = {"say_hello": "00_core.ipynb",
          "multiply": "00_core.ipynb",
          "read_image": "00_core.ipynb",
-         "read_image2": "00_core.ipynb"}
+         "read_image2": "00_core.ipynb",
+         "say_goodbye": "01_export_test.ipynb"}
 
-modules = ["core.py"]
+modules = ["core.py",
+           "export_test.py"]
 
 doc_url = "https://serracb.github.io/nbdev_test/"
 
